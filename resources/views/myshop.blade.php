354900626efcc13b70545a2d4450f4859c7424cd
kainguang <natnicha.thay@kkumail.com>
2024-08-22 17:03:26 +0700

    add login/register

--- resources/views/myshop.blade.php
+++ resources/views/myshop.blade.php
@@ -1,16 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>เกี่ยวกับร้าน IT Shop</h1>
-    <ul>
-        <li>ที่อยู่: {{$address}}</li>
-        <li>เบอร์โทร: {{$mobilephone}}</li>
-        <li>ที่ตั้งร้าน: {{$location}}</li>
-    </ul>
-</body>
-</html>
+@extends('layouts.myapp')
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card">
+                    <h6>เกี่ยวกับร้าน IT Shop</h6>
+                </div>
+                <div class="card-body">
+                    <li>ที่อยู่: {{$address}}</li>
+                    <li>เบอร์โทร: {{$mobilephone}}</li>
+                    <li>ที่ตั้งร้าน: {{$location}}</li>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
